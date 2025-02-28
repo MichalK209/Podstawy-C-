07aba051f1465076dbf5435c02e9ed01d2f50dcc
Michał Kwiatkowski <m.kwiatkowski.839@studms.ug.edu.pl>
2025-02-28 13:04:56 +0100

    final countdown

---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący promień podstawy(r) i wysokość(h) stożka i wypisujący na
+            //ekranie objętość i pole całkowite tego stożka.
+            double r, h, l, V, Pc;
+            Console.WriteLine("Podaj r="); //to co chcemy wyświetlić
+            r = Convert.ToDouble(Console.ReadLine()); //żeby przeczytał to co wpiszemy
+            Console.WriteLine("Podaj h=");
+            h = Convert.ToDouble(Console.ReadLine());
+            l = Math.Sqrt(r * r + h * h); //definiujemy tworzącą
+            V = 1.0 / 3 * Math.PI * r * r*h; //definiujemy objętość
+            Pc = Math.PI * r * r + l* Math.PI * r; //definiujemy pole całkowite
+            Console.WriteLine("Objętość={0}", V); //chcemy żeby zwrócił nam objętość i pole, czyli zdefiniowane wartości po przecinku
+            Console.WriteLine("Pole całkowite={0}", Pc);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanie 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby i wypisujący na ekranie ich sumę, różnicę, iloczyn i
+            //iloraz.
+            double a, b, suma, iloraz, iloczyn, różnica;
+                Console.WriteLine("Podaj a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj b=");
+            b = Convert.ToDouble(Console.ReadLine());
+            suma = a + b;
+            iloraz = a:b;
+            różnica = a - b;
+            iloczyn = a * b;
+            Console.WriteLine();
+        }
+    }
+}
