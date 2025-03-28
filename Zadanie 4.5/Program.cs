1a1343b81564abf9e12987ee5070e3e7ba08c638
Michał Kwiatkowski <m.kwiatkowski.839@studms.ug.edu.pl>
2025-03-28 13:13:08 +0100

    28.03 tablice koniec

---
+++ Zadanie 4.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wczytaj liczbę naturalną n i wypisz na ekranie tą liczbę w systemie dwójkowym.
+            int[] tab = new int[1000]; // Tablica 1000 liczb typu int
+            int i = 0, j, liczba;
+
+            Console.Write("Podaj liczbę naturalną: ");
+            liczba = Convert.ToInt32(Console.ReadLine());
+
+            while ((liczba / 2 != 0) || ((liczba / 2 == 0) && (liczba % 2 != 0)))
+            {
+                tab[i++] = liczba % 2;
+                liczba /= 2;
+            }
+
+            //Wypisuję elementy tablicy w odwrotnej kolejności
+            for (j = i - 1; j >= 0; j--)
+            {
+                Console.Write(tab[j]);
+            }
+
+            Console.ReadKey(true); //ppauza
+        }
+    }
+}
---
+++ Zadanie 4.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadeklaruj tablicę zawierającą nazwy dni tygodnia. Wypisz elementy tablicy za pomocą pętli
+            //foreach.
+            int[] tab = new int[1000];
+            int i = 0, j, liczba;
+
+            Console.Write("Podaj liczbę naturalną: ");
+            liczba = Convert.ToInt32(Console.ReadLine());
+
+            while ((liczba / 2 != 0) || ((liczba / 2 == 0) && (liczba % 2 != 0)))
+            {
+                tab[i++] = liczba % 2;
+                liczba /= 2;
+            }
+            for (j = i - 1; j >= 0; j--)
+            {
+                Console.Write(tab[j]);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
